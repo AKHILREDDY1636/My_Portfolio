b52dc3f6685f778d1e92f0cc20c59264861b2fc0
AKHILREDDY1636 <52082621+AKHILREDDY1636@users.noreply.github.com>
2022-02-23 00:42:57 +0530

    first commit

--- js/script_Internship.js
+++ js/script_Internship.js
@@ -1,7 +1,7 @@
 var internships = [
   "Completed my internship in Cyber Security (CID office, Telangana State Government) for the duration of 45 days<ul><li>Took training on different tools like WireShark</li><li>Played an important role in solving two cyber cases</li></ul>",
   "Completed internship in Amazon (Amazon Development Center, Hyderabad) for durationof 3months. During this internship, I contributed my work for FINTECH team,one of most efficient" +
-    "and crucial team in Developing Teams. <ul><li> Developed internal site using ReactJS.</li><li> Developed an automatic tool in which all the business reports will be directly connectedto WorkDocs.</li></ul> ",
+    "and crucial team in Developing Teams. <ul><li> Developed internal site using ReactJS.</li><li> Developed an automatic tool in which all the business reports will be directly connected to WorkDocs.</li></ul> ",
 ];
 opening_closing_tags = [false, false];
 function onInternship(choice) {
